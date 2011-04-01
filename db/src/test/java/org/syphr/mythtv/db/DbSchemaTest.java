7a863cdc8e92c98b2d76586a749a6b49eef032f2
Gregory Moyer <moyerg@syphr.com>
2011-03-31 23:35:33 -0400

    re-worked the db unit test to focus on grabbing data from the database correctly

---
+++ db/src/test/java/org/syphr/mythtv/db/DbSchemaTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2011 Gregory P. Moyer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.syphr.mythtv.db;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.syphr.mythtv.db.schema.Channel;
+import org.syphr.mythtv.test.Settings;
+import org.syphr.mythtv.test.Utils;
+import org.syphr.prom.PropertiesManager;
+
+public class DbSchemaTest
+{
+    private static PropertiesManager<Settings> settings;
+    private static Logger logger;
+
+    private static EntityManagerFactory factory;
+
+    private EntityManager manager;
+
+    @BeforeClass
+    public static void setupBeforeClass() throws IOException, DatabaseException
+    {
+        settings = Settings.createSettings();
+        logger = LoggerFactory.getLogger(DbSchemaTest.class);
+
+        factory = DbUtils.getEntityManagerFactory(settings.getEnumProperty(Settings.DB_SCHEMA,
+                                                                           SchemaVersion.class),
+                                                  settings.getProperty(Settings.DB_HOST),
+                                                  settings.getIntegerProperty(Settings.DB_PORT),
+                                                  settings.getProperty(Settings.DB_INSTANCE),
+                                                  settings.getProperty(Settings.DB_USER),
+                                                  settings.getProperty(Settings.DB_PASSWORD));
+    }
+
+    @Before
+    public void before()
+    {
+        manager = factory.createEntityManager();
+        manager.getTransaction().begin();
+    }
+
+    @After
+    public void after()
+    {
+        manager.getTransaction().commit();
+        manager.close();
+    }
+
+    @Test
+    public void testChannel()
+    {
+        printFirstFive(Channel.class);
+    }
+
+    private <T> void printFirstFive(Class<T> entityType)
+    {
+        List<T> entities = manager.createQuery("from " + entityType.getName(),
+                                               entityType).getResultList();
+        Utils.printFirstFive(entities, logger);
+    }
+}
