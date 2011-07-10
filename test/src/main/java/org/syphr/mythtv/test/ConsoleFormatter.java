6d92af9f3dbb4d33dc64154aaa2c730d31aed2a0
Gregory Moyer <moyerg@syphr.com>
2011-07-10 10:45:37 -0400

    increased the location column to handle some longer fully qualified class names

--- test/src/main/java/org/syphr/mythtv/test/ConsoleFormatter.java
+++ test/src/main/java/org/syphr/mythtv/test/ConsoleFormatter.java
@@ -30,7 +30,7 @@ public class ConsoleFormatter extends Formatter
     public String format(LogRecord record)
     {
         StringBuilder formattedRecord = new StringBuilder(StringUtils.rightPad(record.getSourceClassName(),
-                                                                               55,
+                                                                               65,
                                                                                '.'));
         formattedRecord.append(" | ");
 
