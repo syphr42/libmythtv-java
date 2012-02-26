ee176efcf47c2468af8645dbc2dcf85fbd995c95
Gregory Moyer <moyerg@syphr.com>
2012-02-26 16:57:22 -0500

    greatly simplified the implementation of spawning a new live tv session

--- api/src/main/java/org/syphr/mythtv/api/backend/Recorder.java
+++ api/src/main/java/org/syphr/mythtv/api/backend/Recorder.java
@@ -17,19 +17,12 @@ package org.syphr.mythtv.api.backend;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.URI;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
 import org.syphr.mythtv.commons.exception.CommandException;
 import org.syphr.mythtv.data.Channel;
 import org.syphr.mythtv.data.Program;
-import org.syphr.mythtv.db.schema.Recorded;
-import org.syphr.mythtv.db.schema.TvChain;
 import org.syphr.mythtv.protocol.Protocol;
 import org.syphr.mythtv.protocol.QueryRecorder;
 import org.syphr.mythtv.protocol.QueryRemoteEncoder;
@@ -71,66 +64,26 @@ public class Recorder
         return queryRemoteEncoder.isBusy((int)unit.toSeconds(duration)).getLeft();
     }
 
-    public RecordingByteChannel startLiveTv(Channel channel) throws IOException,
-                                                            CommandException
+    public RecordingByteChannel startLiveTv(Channel channel) throws IOException, CommandException
     {
         if (channel.getNumber() == null)
         {
             channel = queryRecorder.getChannelInfo(channel.getId());
         }
 
-        String chainId = "live-"
-                         + InetAddress.getLocalHost().getHostName()
-                         + "-"
-                         + new Date().getTime();
+        String chainId = "livetv-"
+                + InetAddress.getLocalHost().getHostName()
+                + "-"
+                + new Date().getTime();
         queryRecorder.spawnLiveTv(chainId, false, channel);
 
-        EntityManager entityMan = backend.getDatabase().getEntityManager();
-        entityMan.getTransaction().begin();
-
-        try
+        if (!queryRecorder.isRecording())
         {
-            TypedQuery<TvChain> tvChainsQuery = entityMan.createQuery("select t from "
-                                                                              + TvChain.class.getName()
-                                                                              + " as t where t.chainid = '"
-                                                                              + chainId
-                                                                              + "' order by chainpos desc",
-                                                                      TvChain.class);
-            List<TvChain> tvChains = tvChainsQuery.getResultList();
-            if (tvChains.isEmpty())
-            {
-                throw new CommandException("Live TV did not start properly");
-            }
-            TvChain latestChain = tvChains.get(0);
-
-            TypedQuery<Recorded> recordedQuery = entityMan.createQuery("select r from "
-                                                                      + Recorded.class.getName()
-                                                                      + " as r where r.id.chanid = '"
-                                                                      + latestChain.getId().getChanid()
-                                                                      + "' and r.id.starttime = '"
-                                                                      + latestChain.getId().getStarttime()
-                                                                      + "'",
-                                                              Recorded.class);
-            List<Recorded> recordedList = recordedQuery.getResultList();
-            if (recordedList.size() != 1)
-            {
-                throw new CommandException("Live TV did not start properly");
-            }
-            Recorded liveTvRecorded = recordedList.get(0);
-
-            Program program = new Program(channel,
-                                                  URI.create(latestChain.getHostprefix() + liveTvRecorded.getBasename()),
-                                                  latestChain.getId()
-                                                             .getStarttime());
-
-            // TODO backend is returning file size 0 for live tv - not sure why yet
-            return new RecordingByteChannel(protocol, program, false, 3000);
+            throw new CommandException("LiveTV failed to start");
         }
-        finally
-        {
-            entityMan.getTransaction().commit();
-            entityMan.close();
-        }
+
+        Program program = queryRecorder.getCurrentRecording();
+        return new RecordingByteChannel(protocol, program, false, 3000);
     }
 
     public void stopLiveTv() throws IOException, CommandException
